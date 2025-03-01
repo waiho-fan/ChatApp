87c4534d6323489799ef577f4ea25b1060e72f97
Wai Ho Fan <waihofan.419@gmail.com>
2025-03-01 11:34:02 +0800

    Initial Commit

---
+++ ChatApps/ChatAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppsApp.swift
+//  ChatApps
+//
+//  Created by Gary on 1/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
